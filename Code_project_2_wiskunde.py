5e405ea58b3c885e10e1ec6336ba40add2c58a46
Silvan778 <silvan.van.der.leij@hva.nl>
2026-01-16 15:23:06 +0100

    Update Code_project_2_wiskunde.py

--- Code_project_2_wiskunde.py
+++ Code_project_2_wiskunde.py
@@ -1,7 +1,6 @@
 # modulen
 import numpy as np
 from scipy.signal import place_poles
-from scipy.linalg import solve_continuous_are
 
 # vaste waarden
 ρ = 700 # kg/m3
@@ -15,62 +14,24 @@ Icm = (1/12) * mp * (d**2 + l**2)
 I = Icm + mp * (l/2)**2
 
 # berekende waarden 2
-a = (3/2) * mp * g * l
-b = I + mp * l**2
-c = (mp*l**2) / 2
-d = Icm + (1/4) * mp * l**2
+a = I + mp * l**2
+b = (mp*l**2) / 2
+c = Icm + (1/4) * mp * l**2
 
-# Matrix M1, M2, M3 berekenen
-M1 = np.array([[1, 0, 0, 0],
-             [0,b+c, 0, c],
-             [0,0,1,0],
-             [0,c+d,0,d]
-])
+# berekende waarden van A en B matrix
+A = np.array([[0, 0, 1, 0],
+             [0, 0, 0, 1],
+             [20.99, -20.97, 0, 0],
+             [-28.03, 76.88, 0, 0]])
 
-M2 = np.array([[0, 1, 0, 0],
-             [a, 0, c, 0],
-             [0,0,0,1],
-             [c,0,c,0]
-])
-
-M3 = np.array([[0],
-             [1],
+B = np.array([[0],
              [0],
-             [0]
-])
-
-# Matrix A en B berekenen
-A = np.linalg.inv(M1) @ M2
-B = np.linalg.inv(M1) @ M3
-
-# Eigenwaarde printen
-eig_A = np.linalg.eigvals(A)
-# eig_B = np.linalg.eigvals(B)
-
-print(f"Eigenwaarden van A: {eig_A}\n")
+             [28.32],
+             [-70.89]])
 
 # gain matrix bepalen met polen
-Polen = np.array([-0.5,-0.6,-0.7,-100]) # zelf gekozen polen / eigenwaarde
+Polen = np.array([-0.5,-10,-11,-100]) # zelf gekozen polen / eigenwaarde
 Geplaatste_Polen = place_poles(A, B, Polen)
 
 K = Geplaatste_Polen.gain_matrix
 print(f"Gain matrix K (1x4):\n {K}")
-
-Q = np.diag([1,    10,   10,   10])
-
-# R: straf op input
-R = np.array([[1]])
-
-# =========================
-# LQR berekening
-# =========================
-P = solve_continuous_are(A, B, Q, R)
-K = np.linalg.inv(R) @ B.T @ P
-
-print("LQR gain matrix K:")
-print(K)
-
-# gesloten-lus polen
-Acl = A - B @ K
-eigvals = np.linalg.eigvals(Acl)
-print("Closed-loop poles:", eigvals)
